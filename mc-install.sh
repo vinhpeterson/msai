78fe4587f6418a4d92f6d92a16110a23768123a5
vinhpeterson <92011258+vinhpeterson@users.noreply.github.com>
2021-11-21 16:55:56 -0800

    minor edits and some color

--- mc-install.sh
+++ mc-install.sh
@@ -2,7 +2,7 @@
 #goes to home directory
 cd
 #creates and goes into directory
-mkdir mc-server
+mkdir ~/mc-server
 cd mc-server
 #updates computer
 sudo apt-get update && sudo apt-get upgrade-y
@@ -14,7 +14,7 @@ sudo apt-get install openjdk-16-jdk -y
 #installs git if not already installed. then clones paper.jar
 sudo apt-get install git -y
 wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/308/downloads/paper-1.17.1-308.jar
-#running the jar file that will generate your needed files, but you will need to change eula.txt. now you can clone your own eula.txt and replace the old one with the new one.
+#runs server jar file and replaces eula
 java -jar paper-1.17.1-308.jar nogui
 sudo rm -r eula.txt
 sudo git clone https://github.com/vinhpeterson/presetup-eula.git
@@ -23,4 +23,6 @@ sudo mv eula.txt ~/mc-server
 cd
 cd mc-server
 # the server should be setup and ready to run
+GREEN='\033[0;32m'
+printf "${GREEN}"
 echo "Your server is setup! Run the command sudo java -jar paper-1.17.1-308.jar nogui to get your server running or run the mc-startup.sh"
