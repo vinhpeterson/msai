edee07eacd4ccba89f227e42016f9faae20a9e0e
vinhpeterson <92011258+vinhpeterson@users.noreply.github.com>
2022-01-16 20:43:09 -0800

    Updates
    Updated jar to 1.18 and java to 17

--- mc-install.sh
+++ mc-install.sh
@@ -9,13 +9,13 @@ sudo apt-get update && sudo apt-get upgrade-y
 sudo apt update
 sudo apt upgrade
 sudo apt autoremove
-#installs java16
-sudo apt-get install openjdk-16-jdk -y
+#installs java17
+sudo apt-get install openjdk-17-jdk -y
 #installs git if not already installed. then clones paper.jar
 sudo apt-get install git -y
-wget https://papermc.io/api/v2/projects/paper/versions/1.17.1/builds/308/downloads/paper-1.17.1-308.jar
+wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/155/downloads/paper-1.18.1-155.jar
 #runs server jar file and replaces eula
-java -jar paper-1.17.1-308.jar nogui
+java -jar paper-1.18.1-155.jar nogui
 sudo rm -r eula.txt
 sudo git clone https://github.com/vinhpeterson/presetup-eula.git
 cd presetup-eula
@@ -26,4 +26,4 @@ sudo rm -r presetup-eula
 # the server should be setup and ready to run
 GREEN='\033[0;32m'
 printf "${GREEN}"
-echo "Your server is setup! Run the command sudo java -jar paper-1.17.1-308.jar nogui to get your server running or run the mc-startup.sh"
+echo "Your server is setup! Run the command sudo java -jar paper-1.18.1-155.jar nogui to get your server running or run the mc-startup.sh"
